e82e90cd32fe46c1d89dfd9d81f805396bbebac3
Nick DeRobertis <whoopnip@gmail.com>
2022-10-08 15:59:10 -0400

    feat: Implement creating router from app import path

--- typer_router/from_fs.py
+++ typer_router/from_fs.py
@@ -1,3 +1,5 @@
+import importlib
+from pathlib import Path
 from typing import TYPE_CHECKING, List
 
 if TYPE_CHECKING:
@@ -6,5 +8,27 @@ if TYPE_CHECKING:
 
 def routes_from_app_import_path(app_import_path: str) -> List["Route"]:
     """Create routes from an app import path."""
+    from typer_router.route import Route
 
-    return []
+    app_path = _app_folder_from_import_path(app_import_path)
+    # Iterate through nested directories and files
+    routes: List[Route] = []
+    for path in app_path.rglob("*"):
+        if path.is_dir():
+            continue
+        if path.name == "__init__.py":
+            continue
+        if not path.name.endswith(".py"):
+            continue
+        relative_path = path.relative_to(app_path)
+        route = Route.from_file_path(relative_path)
+        routes.append(route)
+
+    return routes
+
+
+def _app_folder_from_import_path(app_import_path: str) -> Path:
+    """Get the app folder from an app import path."""
+    app_module = importlib.import_module(app_import_path)
+    app_folder = Path(app_module.__file__).parent
+    return app_folder
